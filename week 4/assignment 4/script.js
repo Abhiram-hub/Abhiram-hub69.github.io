9dfda51475fe243e33a816aef3f14f740017268d
Abhiram-hub <79072197+Abhiram-hub@users.noreply.github.com>
2021-03-31 18:43:46 +0530

    Update script.js

--- week 4/assignment 4/script.js
+++ week 4/assignment 4/script.js
@@ -1,21 +1,21 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
+names[0]="yakoov";
+names[1]="jhon";
 names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
 names[5]="frank";
 names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[7]="parul";
+names[8]="jin";
+names[9]="mohith";
 
 
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
+for (var i = 0; i< names.length; i++){
+    if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("goodbye" , names[i])
+    }
+    else{
+        console.log("hello", names[i])
+    }
 }
